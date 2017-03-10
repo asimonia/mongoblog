cff2bc57876ffb8673d093cbb3630c56c3cbed88
asimonia <alex.simonian@gmail.com>
2017-03-09 21:51:30 -0600

    Complete blog

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from database import Database
-from models.post import Post
-from models.blog import Blog
+from menu import Menu
 
 Database.initialize()
 
-blog = Blog(author="Alex",
-			title="Sample",
-			description="Sample description")
+menu = Menu()
 
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
+menu.run_menu()
--- database.py
+++ database.py
@@ -1,27 +1,23 @@
 import pymongo
 
+
 class Database:
-	URI = 'mongodb://127.0.0.1:27017'
-	DATABASE = None
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
 
-	@staticmethod
-	def initialize():
-		"""Initialize the database"""
-		client = pymongo.MongoClient(Database.URI)
-		Database.DATABASE = client['fullstack']
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['fullstack']
 
-	@staticmethod
-	def insert(collection, data):
-		"""Inserts data into a collection of the database"""
-		Database.DATABASE[collection].insert(data)
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
 
-	@staticmethod
-	def find(collection, query):
-		"""Returns document from a collection"""
-		Database.DATABASE[collection].find(query)
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
 
-	@staticmethod
-	def find_one(collection, query):
-		"""Return a row from a document in a collection"""
-		Database.DATABASE[collection].find_one(query)
-
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
